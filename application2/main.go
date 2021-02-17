8e6235ba0f6b9706f80a5763c4cb19c31fb5e47d
Harsha m <hm@onetrust.com>
2021-02-17 11:35:12 +0530

    new files

---
+++ application2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	fm "fmt"
+
+	app1 "github.com/Harshaec021/appliaction"
+)
+
+func main() {
+	fm.Println(app1.Name)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Harshaec021/application2
+
+go 1.15
